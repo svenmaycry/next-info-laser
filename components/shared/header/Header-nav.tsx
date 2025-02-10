84adefa3b34ed062a651b625cb82308e77ec28af
SvenMayCry <svenmaycry@gmail.com>
2025-02-10 17:38:36 +0300

    Спойлеры работают по клику и по наведению. Лок боди не работает, если меню открыто. Закрытие меню при клике вне меню

--- components/shared/header/Header-nav.tsx
+++ components/shared/header/Header-nav.tsx
@@ -1,14 +1,28 @@
 "use client";
 
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Link from "next/link";
 import {HeaderProductItem} from "@/components/shared/header/nav-items/Header-product-item";
 import {cn} from "@/lib/utils";
 import {Menu, X} from "lucide-react";
 import {Overlay} from "@/components/shared/Overlay";
+import {useClickAway} from "react-use";
 
 export const HeaderNav: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navRef = useRef(null);
+  const menuRef = useRef<HTMLButtonElement | null>(null);
+
+  useClickAway(navRef, (event: MouseEvent | TouchEvent) => {
+    const target = event.target as Node | null;
+
+    if (menuRef.current && target && menuRef.current.contains(target)) {
+      return
+    }
+
+    setIsMenuOpen(false);
+  });
+
 
   useEffect(() => {
     if (isMenuOpen) {
@@ -27,49 +41,76 @@ export const HeaderNav: React.FC = () => {
 
       {/* Оверлей */}
       <Overlay isOpen={isMenuOpen}/>
-      
+
       <nav
+        ref={navRef}
         className={cn(
           "bg-white ml-auto mr-4",
-          "max-xl:fixed max-xl:top-20 max-xl:left-[-110%] max-xl:h-full max-xl:w-2/3 max-xl:transition-all max-xl:ease-in-out max-xl:duration-300 max-xl:shadow-lg",
-          "",
+          "max-xl:fixed max-xl:top-20 max-xl:left-[-110%] max-xl:h-full max-xl:w-2/3 max-xl:transition-all max-xl:ease-in-out max-xl:duration-300 max-xl:shadow-lg max-xl:border-t max-xl:border-t-gray-300",
           isMenuOpen && "max-xl:left-0"
         )}
       >
         <ul className={cn(
           "flex flex-row items-center",
-          "max-xl: max-xl:flex-col max-xl:items-start"
+          "max-xl:max-xl:flex-col max-xl:items-start"
         )}>
 
           <HeaderProductItem/>
 
           <li>
-            <Link className="block px-2 hover:text-[#6941f9] transition-colors" href={"/about"}>
+            <Link
+              className={cn(
+                'max-xl:w-full max-xl:justify-between max-xl:font-bold',
+                'block px-2 hover:text-[#6941f9] transition-colors'
+              )}
+
+              href={"/about"}
+            >
               О компании
             </Link>
           </li>
           <li>
-            <Link className="block px-2 hover:text-[#6941f9] transition-colors" href={"/clients"}>
+            <Link className={cn(
+              'max-xl:w-full max-xl:justify-between max-xl:font-bold',
+              "block px-2 hover:text-[#6941f9] transition-colors"
+            )}
+                  href={"/clients"}>
               Клиенты
             </Link>
           </li>
           <li>
-            <Link className="block px-2 hover:text-[#6941f9] transition-colors" href={"/articles"}>
+            <Link className={cn(
+              'max-xl:w-full max-xl:justify-between max-xl:font-bold',
+              "block px-2 hover:text-[#6941f9] transition-colors"
+            )}
+                  href={"/articles"}>
               Статьи
             </Link>
           </li>
           <li>
-            <Link className="block px-2 hover:text-[#6941f9] transition-colors" href={"/delivery"}>
+            <Link className={cn(
+              'max-xl:w-full max-xl:justify-between max-xl:font-bold',
+              "block px-2 hover:text-[#6941f9] transition-colors"
+            )}
+                  href={"/delivery"}>
               Доставка и оплата
             </Link>
           </li>
           <li>
-            <Link className="block px-2 hover:text-[#6941f9] transition-colors" href={"/contacts"}>
+            <Link className={cn(
+              'max-xl:w-full max-xl:justify-between max-xl:font-bold',
+              "block px-2 hover:text-[#6941f9] transition-colors"
+            )}
+                  href={"/contacts"}>
               Контакты
             </Link>
           </li>
           <li>
-            <Link className="block px-2 hover:text-[#6941f9] transition-colors" href={"/service"}>
+            <Link className={cn(
+              'max-xl:w-full max-xl:justify-between max-xl:font-bold',
+              "block px-2 hover:text-[#6941f9] transition-colors"
+            )}
+                  href={"/service"}>
               Сервис
             </Link>
           </li>
@@ -78,6 +119,7 @@ export const HeaderNav: React.FC = () => {
 
       {/* Бургер кнопка */}
       <button
+        ref={menuRef}
         className={cn(
           "xl:hidden",
           "max-xl:block max-xl:bg-gray-200 max-xl::hover:bg-gray-300 max-xl::transition max-xl:p-1 max-xl:rounded-md"
--- components/shared/header/nav-items/Header-product-item.tsx
+++ components/shared/header/nav-items/Header-product-item.tsx
@@ -5,13 +5,16 @@ import {Container} from "@/components/shared/Container";
 import {cn} from "@/lib/utils";
 import Link from "next/link";
 import {Overlay} from "@/components/shared/Overlay";
-import {Product} from "@/types/product";
 import {ChevronDown} from "lucide-react";
 import {getAllProducts, getCategories} from "@/lib/api";
 import {Category} from "@/types/category";
+import {useMedia} from "react-use";
+import {Product} from "@/types/product";
 
 export const HeaderProductItem: React.FC = () => {
-  
+
+  const isMobile = useMedia("(max-width: 1280px)");
+
   const [products, setProducts] = useState<Product[]>([]);
   const [categories, setCategories] = useState<Category[]>([]);
   const [isSpoilerOpen, setIsSpoilerOpen] = useState(false);
@@ -29,31 +32,36 @@ export const HeaderProductItem: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if (isSpoilerOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
+    if (!isMobile) {
+      document.body.style.overflow = isSpoilerOpen ? "hidden" : "";
     }
 
     return () => {
-      document.body.style.overflow = "";
+      if (!isMobile) {
+        document.body.style.overflow = "";
+      }
     };
-  }, [isSpoilerOpen]);
+  }, [isSpoilerOpen, isMobile]);
+
 
   return (
     <>
-      <Overlay isOpen={isSpoilerOpen}/>
+      <Overlay isOpen={!isMobile && isSpoilerOpen}/>
 
-      <li onMouseLeave={() => setIsSpoilerOpen(false)}>
+      <li
+        onMouseLeave={() => setIsSpoilerOpen(false)}
+        className={cn('max-xl:w-full')}
+      >
         <button
           type="button"
           className={cn(
-            "relative flex items-center gap-x-1 rounded-md bg-gray-400/30 hover:text-[#6941f9] transition-colors duration-300 px-3 py-1",
-            isSpoilerOpen
-              ? "text-[#6941f9] bg-gray-400/50 before:content-[] before:absolute before:left-0 before:bottom-[-40px] before:h-[40px] before:w-full"
-              : ""
+            "relative flex items-center gap-x-1 rounded-md xl:bg-gray-400/30 hover:text-[#6941f9] transition-colors duration-300 px-3 lg:py-1",
+            "lg:before:content-[] lg:before:absolute lg:before:left-0 lg:before:bottom-[-40px] lg:before:h-[40px] lg:before:w-full",
+            "max-xl:w-full max-xl:justify-between max-xl:font-bold max-xl:px-2",
+            isSpoilerOpen ? "text-[#6941f9] xl:bg-gray-400/50 " : "",
           )}
-          onMouseEnter={() => setIsSpoilerOpen(true)}
+          onMouseEnter={() => !isMobile && setIsSpoilerOpen(true)}
+          onClick={() => isMobile && setIsSpoilerOpen(!isSpoilerOpen)}
         >
           Каталог
           <ChevronDown
@@ -67,13 +75,16 @@ export const HeaderProductItem: React.FC = () => {
 
         <div
           className={cn(
-            "absolute top-[64px] left-0 right-0 bg-white border-t border-t-gray-300  transition-all duration-300 ease-in-out z-30",
+            "absolute top-[64px] left-0 right-0 bg-white xl:border-t xl:border-t-gray-300  transition-all duration-300 ease-in-out z-30",
+            "max-xl:static max-xl:overflow-hidden",
+            isSpoilerOpen ? "max-xl:max-h-full" : "max-xl:max-h-0",
             isSpoilerOpen ? "visible opacity-100" : "invisible opacity-0"
           )}
         >
           <Container
             className={cn(
-              "relative py-5 transition-all duration-300 ease-in-out",
+              "relative xl:py-5 transition-all duration-300 ease-in-out",
+              "max-lg: px-4",
               isSpoilerOpen ? "top-0 " : "-top-3"
             )}
           >
